819167d791cdeeaaa31e45dee0ac8b5e1dea5133
MARIUM BINTE MUHAMMAD <165379776+marium0908@users.noreply.github.com>
2025-04-20 14:50:51 +0600

    Add files via upload

---
+++ send-message.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name    = htmlspecialchars($_POST["name"]);
+    $email   = htmlspecialchars($_POST["email"]);
+    $subject = htmlspecialchars($_POST["subject"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $to = "mariumbintemuhammmad@gmail.com"; // Your email
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
+        header("Location: thank-you.html");
+        exit();
+    } else {
+        echo "Sorry, something went wrong. Please try again later.";
+    }
+} else {
+    header("Location: contact.html");
+    exit();
+}
+?>
